3eb1f17c7b771e0835c48b2c770ffcbe7a17b1b6
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2021-10-24 03:19:37 +0530

    Payment gateway backend
    Payment gateway backend

--- Customer/ApplyforJob-Responses/posted_ad.php
+++ Customer/ApplyforJob-Responses/posted_ad.php
@@ -2,7 +2,7 @@
 
     $con = require '../../Backend/db_connection.php';
 
-    $query = "SELECT category, title, job, budget FROM customer_ads";
+    $query = "SELECT ads_id, category, title, job, budget FROM customer_ads";
 
     $result = mysqli_query($con, $query);
 
