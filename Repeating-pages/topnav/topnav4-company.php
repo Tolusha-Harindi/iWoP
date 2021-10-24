8dcaaa4d89aabf24e2b5508a47f3419a7d586d72
Imashi Dharmawardana <imashidharmawardana@gmail.com>
2021-10-24 11:29:44 +0530

    change wanted ads page

--- Repeating-pages/topnav/topnav4-company.php
+++ Repeating-pages/topnav/topnav4-company.php
@@ -15,8 +15,8 @@
                 <input type="image" id="image" alt="Landing" src="../../Images/Logo.jpg">
             </div>
             <a href="../../Landing/Landing.php"> <b>| Logout  </b></a> 
-            <a href="#help"> <b>| Help page  </b></a> 
-            <a href="#category"><b> | Category </b></a> 
+            <a href="../../Customer/FAQ/FAQ.php"> <b>| Help page  </b></a> 
+            <a href="#"><b> | Category </b></a> 
             <a href="#alladds"> <b>| All Ads </b></a> 
             <a href="../../Company/CompanyProfile/companyprofile.php"><i class="fa fa-user-circle-o fa-2x"></i> </a>
         </div>
