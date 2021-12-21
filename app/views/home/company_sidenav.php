7df1ca8d885864cb16e5d21ccce71611b39f48f0
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2021-12-21 14:28:54 +0530

    company side nav

--- app/views/home/company_sidenav.php
+++ app/views/home/company_sidenav.php
@@ -2,7 +2,7 @@
 <link rel='stylesheet' href="../../../public/css/home/sidenav.css"/>
 
 
-    <a href="../company/company_profile.php" class='card-link'>
+    <a href="../companies/company_profile.php" class='card-link'>
         <div class="card" style ="width: 15vm;">
             <div class="img-container">
                 <img src="../../../public/img/comp.png" alt="propic" class="img1"/>
@@ -19,27 +19,27 @@
     
     <div class="sidebar">
         <div class="description">
-        <a href="../company/company_dashboard.php" class='side-link-active'>
+        <a href="../companies/company_dashboard.php" class='side-link-active'>
                 <ul class='side-list-active'>
                     <li>Dashboard</li>
                 </ul>
         </a>
-        <a href="../company/company_ads.php" class='side-link'>
+        <a href="../companies/company_ads.php" class='side-link'>
                 <ul class='side-list'>
                     <li>Responses</li>
                 </ul>
         </a>
-        <a href="../company/company_category.php" class='side-link'>
+        <a href="../companies/company_category.php" class='side-link'>
                 <ul class='side-list'>
                     <li>Category</li>
                 </ul>
         </a>
-        <a href="../company/company_inviteforjob.php" class='side-link'>
+        <a href="../companies/company_inviteforjob.php" class='side-link'>
                 <ul class='side-list'>
                     <li>Service providers</li>
                 </ul>
         </a>
-        <a href="../company/company_messagee.php" class='side-link'>
+        <a href="../companies/company_messagee.php" class='side-link'>
                 <ul class='side-list'>
                     <li>Message</li>
                 </ul>
