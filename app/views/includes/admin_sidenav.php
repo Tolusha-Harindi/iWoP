a2f6d5b618e6c86cc97afa7e582fd10671131f7b
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2021-12-22 01:10:46 +0530

    admin sidenav in mvc

--- app/views/includes/admin_sidenav.php
+++ app/views/includes/admin_sidenav.php
@@ -19,37 +19,37 @@
     
         <div class="sidebar">
             <div class="description">
-            <a href="<?php echo URLROOT;?>admins/admin_dashboard" class='side-link-active'>
+            <a href="<?php echo URLROOT;?>/admins/admin_dashboard" class='side-link-active'>
                     <ul class='side-list-active'>
                         <li>Dashboard</li>
                     </ul>
             </a>
-            <a href="<?php echo URLROOT;?>admins/admin_manager" class='side-link'>
+            <a href="<?php echo URLROOT;?>/admins/admin_manager" class='side-link'>
                     <ul class='side-list'>
                         <li>Manager</li>
                     </ul>
             </a>
-            <a href="<?php echo URLROOT;?>admins/admin_category" class='side-link'>
+            <a href="<?php echo URLROOT;?>/admins/admin_category" class='side-link'>
                     <ul class='side-list'>
                         <li>Category</li>
                     </ul>
             </a>
-            <a href="<?php echo URLROOT;?>admins/admin_pendingads" class='side-link'>
+            <a href="<?php echo URLROOT;?>/admins/admin_pendingads" class='side-link'>
                     <ul class='side-list'>
                         <li>Pending Ads</li>
                     </ul>
             </a>
-            <a href="<?php echo URLROOT;?>admins/admin_complaints" class='side-link'>
+            <a href="<?php echo URLROOT;?>/admins/admin_complaints" class='side-link'>
                     <ul class='side-list'>
                         <li>Complaints</li>
                     </ul>
             </a>
-            <a href="<?php echo URLROOT;?>admin_faq" class='side-link'>
+            <a href="<?php echo URLROOT;?>/admins/admin_faq" class='side-link'>
                     <ul class='side-list'>
                         <li>FAQ</li>
                     </ul>
             </a>
-            <a href="<?php echo URLROOT;?>admin_registered_users" class='side-link'>
+            <a href="<?php echo URLROOT;?>/admins/admin_registered_users" class='side-link'>
                     <ul class='side-list'>
                         <li>Registered Users</li>
                     </ul>
