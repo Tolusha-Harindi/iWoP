fc227c7cbafd12c6f7735839f8a2087dee25ae63
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2021-12-23 00:49:38 +0530

    Create iwop.sql

---
+++ iwop.sql
@@ -0,0 +1,654 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2021 at 07:23 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iwop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `name` varchar(50) NOT NULL,
+  `nic` varchar(12) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `prof_pic` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads`
+--
+
+CREATE TABLE `ads` (
+  `ads_id` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `post_date` date NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `decription` mediumtext NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `budget` float NOT NULL,
+  `work_style` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE `bank` (
+  `bank_id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `holder` varchar(50) NOT NULL,
+  `bank_name` varchar(50) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `code` int(10) NOT NULL,
+  `account_no` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category` varchar(30) NOT NULL,
+  `logo` blob NOT NULL,
+  `add_date` date NOT NULL,
+  `cat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `reg_no` varchar(20) NOT NULL,
+  `com_name` varchar(50) NOT NULL,
+  `owner_name` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `prof_pic` blob NOT NULL,
+  `experience` int(11) NOT NULL,
+  `gs_certi` blob NOT NULL,
+  `reg_certi` blob NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cus_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `prof_pic` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faq`
+--
+
+CREATE TABLE `faq` (
+  `faq_id` int(11) NOT NULL,
+  `question` mediumtext NOT NULL,
+  `answer` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `job_id` int(11) NOT NULL,
+  `reg_no` varchar(20) NOT NULL,
+  `post_date` date NOT NULL,
+  `category` varchar(25) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `salary` float NOT NULL,
+  `duration` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `prof_pic` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `past_experience`
+--
+
+CREATE TABLE `past_experience` (
+  `service_id` int(11) NOT NULL,
+  `experience` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `pay_id` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `rep_id` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `reg_no` varchar(20) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `reason` varchar(50) NOT NULL,
+  `comment` mediumtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `reg_no` varchar(20) CHARACTER SET utf8mb4 NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `sender` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
+  `review` text CHARACTER SET utf8mb4 NOT NULL,
+  `ratings` smallint(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `schedule_id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `cus_name` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `service_id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `gs_certi` blob NOT NULL,
+  `exp_year` int(11) NOT NULL,
+  `work_hour` int(11) NOT NULL,
+  `call_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_category`
+--
+
+CREATE TABLE `service_category` (
+  `service_id` int(11) NOT NULL,
+  `category` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_qualification`
+--
+
+CREATE TABLE `service_qualification` (
+  `service_id` int(11) NOT NULL,
+  `qualification` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worker`
+--
+
+CREATE TABLE `worker` (
+  `worker_id` int(11) NOT NULL,
+  `nic` varchar(12) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `district` varchar(25) NOT NULL,
+  `city` varchar(25) NOT NULL,
+  `nic_front` blob NOT NULL,
+  `nic_back` blob NOT NULL,
+  `prof_pic` blob NOT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `working_city`
+--
+
+CREATE TABLE `working_city` (
+  `service_id` int(11) NOT NULL,
+  `city` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `working_days`
+--
+
+CREATE TABLE `working_days` (
+  `service_id` int(11) NOT NULL,
+  `day` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `working_district`
+--
+
+CREATE TABLE `working_district` (
+  `service_id` int(11) NOT NULL,
+  `district` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`nic`);
+
+--
+-- Indexes for table `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`ads_id`),
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`bank_id`),
+  ADD KEY `worker_id` (`worker_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`reg_no`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cus_id`);
+
+--
+-- Indexes for table `faq`
+--
+ALTER TABLE `faq`
+  ADD PRIMARY KEY (`faq_id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`job_id`),
+  ADD KEY `reg_no` (`reg_no`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `past_experience`
+--
+ALTER TABLE `past_experience`
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`pay_id`),
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`rep_id`),
+  ADD KEY `cus_id` (`cus_id`),
+  ADD KEY `reg_no` (`reg_no`),
+  ADD KEY `worker_id` (`worker_id`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`review_id`),
+  ADD KEY `cus_id` (`cus_id`),
+  ADD KEY `worker_id` (`worker_id`),
+  ADD KEY `reg_no` (`reg_no`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`schedule_id`),
+  ADD KEY `worker_id` (`worker_id`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`service_id`),
+  ADD KEY `worker_id` (`worker_id`);
+
+--
+-- Indexes for table `service_category`
+--
+ALTER TABLE `service_category`
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `service_qualification`
+--
+ALTER TABLE `service_qualification`
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `worker`
+--
+ALTER TABLE `worker`
+  ADD PRIMARY KEY (`worker_id`);
+
+--
+-- Indexes for table `working_city`
+--
+ALTER TABLE `working_city`
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `working_days`
+--
+ALTER TABLE `working_days`
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `working_district`
+--
+ALTER TABLE `working_district`
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `ads_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `bank_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `faq`
+--
+ALTER TABLE `faq`
+  MODIFY `faq_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `pay_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `rep_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `worker`
+--
+ALTER TABLE `worker`
+  MODIFY `worker_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ads`
+--
+ALTER TABLE `ads`
+  ADD CONSTRAINT `ads_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`);
+
+--
+-- Constraints for table `bank`
+--
+ALTER TABLE `bank`
+  ADD CONSTRAINT `bank_ibfk_1` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`worker_id`);
+
+--
+-- Constraints for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`reg_no`) REFERENCES `company` (`reg_no`);
+
+--
+-- Constraints for table `past_experience`
+--
+ALTER TABLE `past_experience`
+  ADD CONSTRAINT `past_experience_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`);
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`),
+  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`reg_no`) REFERENCES `company` (`reg_no`),
+  ADD CONSTRAINT `report_ibfk_3` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`worker_id`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`),
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`worker_id`),
+  ADD CONSTRAINT `review_ibfk_3` FOREIGN KEY (`reg_no`) REFERENCES `company` (`reg_no`);
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`worker_id`);
+
+--
+-- Constraints for table `service`
+--
+ALTER TABLE `service`
+  ADD CONSTRAINT `service_ibfk_1` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`worker_id`);
+
+--
+-- Constraints for table `service_category`
+--
+ALTER TABLE `service_category`
+  ADD CONSTRAINT `service_category_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`);
+
+--
+-- Constraints for table `service_qualification`
+--
+ALTER TABLE `service_qualification`
+  ADD CONSTRAINT `service_qualification_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`);
+
+--
+-- Constraints for table `working_city`
+--
+ALTER TABLE `working_city`
+  ADD CONSTRAINT `working_city_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`);
+
+--
+-- Constraints for table `working_days`
+--
+ALTER TABLE `working_days`
+  ADD CONSTRAINT `working_days_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`);
+
+--
+-- Constraints for table `working_district`
+--
+ALTER TABLE `working_district`
+  ADD CONSTRAINT `working_district_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
