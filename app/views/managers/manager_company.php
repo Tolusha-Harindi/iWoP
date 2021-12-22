17e583a50b3fb7e955207c7b6b61461c39ce5756
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2021-12-22 20:36:20 +0530

    Manager  Comapny page in MVC

--- app/views/managers/manager_company.php
+++ app/views/managers/manager_company.php
@@ -1,9 +1,9 @@
-<?php include("../home/topnav.php"); ?>
+<?php include_once APPROOT . '/views/includes/topnav.php'; ?>
 
-<?php include("../home/manager_sidenav.php"); ?>
+<?php  include_once APPROOT . '/views/includes/manager_sidenav.php'; ?>
 
-<link rel='stylesheet' href="../../../public/css/home/button.css"/>
-<link rel='stylesheet' href="../../../public/css/home/table.css"/>
+<link rel='stylesheet' href="<?php echo URLROOT;?> /public/css/home/button.css"/>
+<link rel='stylesheet' href="<?php echo URLROOT;?> /public/css/home/table.css"/>
 <link href="https://fonts.googleapis.com/css2?family=Rancho&display=swap" rel="stylesheet">
 
 
@@ -26,7 +26,7 @@
                 <tbody>
                 
                     <tr>
-                        <td><img src="../../../public/img/w1.jpg" class="table-image" style="border-radius: 50px;"></td>
+                        <td><img src="<?php echo URLROOT;?> /public/img/w1.jpg" class="table-image" style="border-radius: 50px;"></td>
                         <td>Dream Wave</td>
                         <td>10/10/2021</td>
                         <!-- <td>2h ago </td> -->
@@ -34,7 +34,7 @@
                     </tr>
 
                     <tr>
-                        <td><img src="../../../public/img/w2.jpg" class="table-image" style="border-radius: 50px;"></td>
+                        <td><img src="<?php echo URLROOT;?> /public/img/w2.jpg" class="table-image" style="border-radius: 50px;"></td>
                         <td>Dream Wave</td>
                         <td>10/10/2021</td>
                         <!-- <td>2h ago </td> -->
@@ -42,7 +42,7 @@
                     </tr>
 
                     <tr>
-                        <td><img src="../../../public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
+                        <td><img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
                         <td>Dream Wave</td>
                         <td>10/10/2021</td>
                         <!-- <td>2h ago </td> -->
@@ -50,7 +50,7 @@
                     </tr>
 
                      <tr>
-                        <td><img src="../../../public/img/admin.jpg" class="table-image" style="border-radius: 50px;"></td>
+                        <td><img src="<?php echo URLROOT;?> /public/img/admin.jpg" class="table-image" style="border-radius: 50px;"></td>
                         <td>Dream Wave</td>
                         <td>10/10/2021</td>
                         <!-- <td>2h ago </td> -->
@@ -58,7 +58,7 @@
                     </tr>
 
                     <tr>
-                        <td><img src="../../../public/img/admin.jpg" class="table-image" style="border-radius: 50px;"></td>
+                        <td><img src="<?php echo URLROOT;?> /public/img/admin.jpg" class="table-image" style="border-radius: 50px;"></td>
                         <td>Dream Wave</td>
                         <td>10/10/2021</td>
                         <!-- <td>2h ago </td> -->
@@ -66,7 +66,7 @@
                     </tr>
 
                     <tr>
-                        <td><img src="../../../public/img/admin.jpg" class="table-image" style="border-radius: 50px;"></td>
+                        <td><img src="<?php echo URLROOT;?> /public/img/admin.jpg" class="table-image" style="border-radius: 50px;"></td>
                         <td>Dream Wave</td>
                         <td>10/10/2021</td>
                         <!-- <td>2h ago </td> -->
@@ -81,7 +81,7 @@
 
 <!---------------------------Arrow button----------------------------------------->
 <div class="arrow">
-	<a href="#"><img src="../../../public/img/arrow.png" alt="up_arrow"></a>
+	<a href="#"><img src="<?php echo URLROOT;?> /public/img/arrow.png" alt="up_arrow"></a>
 </div>  
 
-<?php include("../home/footer.php"); ?>
+<?php include_once APPROOT . '/views/includes/footer.php'; ?>
