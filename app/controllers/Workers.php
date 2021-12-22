0a431f0c2ab2d6375d7466e64b1dfb49aac1afe3
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2021-12-22 21:43:21 +0530

    Worker controller Page

---
+++ app/controllers/Workers.php
@@ -0,0 +1,177 @@
+<?php
+    class Workers extends Controller {
+        public function __construct() {
+            //$this->pageModel = $this->model('Page');
+        }
+
+        public function worker_all_ads() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_all_ads page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_all_ads', $data);
+          
+        }
+
+        public function worker_bank_detail() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_bank_detail page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_bank_detail', $data);
+          
+        }
+
+        public function worker_bank_edit() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_bank_edit page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_bank_edit', $data);
+          
+        }
+
+        public function worker_chat() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_chat page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_chat', $data);
+          
+        }
+
+        public function worker_dashboard() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_dashboard page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_dashboard', $data);
+          
+        }
+
+        public function worker_job_invite() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_job_invite page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_job_invite', $data);
+          
+        }
+
+        public function worker_job_vacanccies() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_job_vacanccies page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_job_vacanccies', $data);
+          
+        }
+
+        public function worker_new_profile() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_new_profile page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_new_profile', $data);
+          
+        }
+
+        public function worker_payment() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_payment page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_payment', $data);
+          
+        }
+
+        public function worker_profile_edit() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_profile_edit page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_profile_edit', $data);
+          
+        }
+
+        public function worker_schedule() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_schedule page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_schedule', $data);
+          
+        }
+
+        public function worker_service() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_service page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_service', $data);
+          
+        }
+
+        public function worker_view_profile() {
+
+            //$users = $this->pageModel-> getUsers();
+            $data = [
+                'title' => 'worker_view_profile page',
+                //'users' => $users
+
+            ];
+
+            $this->view('managers/worker_view_profile', $data);
+          
+        }
+
+
+}
