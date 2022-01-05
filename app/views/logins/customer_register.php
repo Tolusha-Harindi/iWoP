54adf593f411e2d79af8e13442a2b1202194025d
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2022-01-06 00:35:13 +0530

    updated customer register page

--- app/views/logins/customer_register.php
+++ app/views/logins/customer_register.php
@@ -3,7 +3,7 @@
 <link rel='stylesheet' href="<?php echo URLROOT;?>/public/css/home/login.css"/>
 <link rel='stylesheet' href="<?php echo URLROOT;?>/public/css/home/button.css"/>
 
-<div class="shape" style="background-color: #F10B67; height:148.5%;"> </div>
+<div class="shape" style="background-color: #F10B67; height:166%;"> </div>
 <img src="<?php echo URLROOT;?>/public/img/reg-1.png" class="image" alt="" />
 
 <form action="<?php echo URLROOT; ?>/logins/customer_register" method="POST">
@@ -13,43 +13,43 @@
             <input type="text" placeholder="Enter Firstname" name="fname">
             <span class="invalidFeedback">
                 <?php  echo $data['firstnameError'];?>
-            </span>
+            </span> </br> </br>
 
             <label for="lname"><b>Lastname</b></label>
             <input type="text" placeholder="Enter Lastname" name="lname">
             <span class="invalidFeedback">
                 <?php  echo $data['lastnameError'];?>
-            </span>
+            </span> </br> </br>
 
             <label for="contact"><b>Contact Number</b></label>
             <input type="text" placeholder="Enter Contact Number" name="contact">
             <span class="invalidFeedback">
                 <?php  echo $data['contactError'];?>
-            </span> 
+            </span> </br> </br>
 
             <label for="email"><b>Email</b></label>
             <input type="email" placeholder="Enter Email" name="email">
             <span class="invalidFeedback">
                 <?php  echo $data['emailError'];?>
-            </span>
+            </span> </br> </br>
 
             <label for="address"><b>Address</b></label>
             <input type="text" placeholder="Enter Address" name="address">
             <span class="invalidFeedback">
                 <?php  echo $data['addressError'];?>
-            </span>
+            </span> </br> </br>
 
             <label for="password"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="password">
             <span class="invalidFeedback">
                 <?php  echo $data['passwordError'];?>
-            </span>
+            </span> </br> </br>
 
             <label for="re-enterpassword"><b>Re-Enter Password</b></label>
             <input type="password" placeholder="Re-Enter Password" name="re-enterpassword">
             <span class="invalidFeedback">
                 <?php  echo $data['re-enterpasswordError'];?>
-            </span>
+            </span> </br> </br>
 
             <button type="submit" class="blue-button">Sign-Up</button>
             <p class="register-link"><a href="<?php echo URLROOT;?>/logins/login">Back to Login >></a> </p>
