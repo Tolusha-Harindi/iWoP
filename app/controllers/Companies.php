0d03cd628e8aa0b0994bac13848c4d6c01fdb14a
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2022-01-15 16:58:50 +0530

    completed company posting ads, review ads

--- app/controllers/Companies.php
+++ app/controllers/Companies.php
@@ -1,8 +1,8 @@
 <?php
 class Companies extends Controller {
     public function __construct() {
-        $this->reviewModel = $this->model('Review');
-        // $this->companyAddModel = $this->model('CompanyAdd');
+        $this->companyModel = $this->model('Company');
+        
     }
 
     //load the main page related to company
@@ -11,7 +11,7 @@ class Companies extends Controller {
     }
 
     public function company_profilesforworkernew() {
-        $reviews = $this->reviewModel->getReviews();
+        $reviews = $this->companyModel->getReviews();
         echo 'Type of the reviews - '.getType($reviews);
         $fillingData = (Object)[
             'reviewID'=>'',
@@ -54,36 +54,135 @@ class Companies extends Controller {
 
     
      public function company_ads() {
+            if(!isLoggedIn()){
+                header("Location: " . URLROOT . "/companies");
+            }
+            $posts = $this->companyModel->findAllPosts();
+            
+            $data = [
+                'posts' => $posts,
+                'category' => '',
+                'title' => '',
+                'description' => '',
+                'address' => '',
+                'contact' => '',
+                'start_date' => '',
+                'end_date' => '',
+                'budget' => '',
+                'work' => '',
+                'categoryError' => '',
+                'titleError' => '',
+                'descriptionError' => '',
+                'addressError' => '',
+                'contactError' => '',
+                'start_dateError' => '',
+                'end_dateError' => '',
+                'budgetError' => '',
+                'workError' => '',
 
-    
-       
-        $this->view('companies/company_ads', $data); 
+            ];
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'reg_no' => $_SESSION['reg_no'],
+                'category' => trim($_POST['category']),
+                'title' => trim($_POST['title']),
+                'description' => trim($_POST['description']),
+                'address' => trim($_POST['address']),
+                'contact' => trim($_POST['contact']),
+                'start_date' => trim($_POST['start_date']),
+                'end_date' => trim($_POST['end_date']),
+                'budget' => trim($_POST['budget']),
+                'work' => trim($_POST['work']),
+                'categoryError' => '',
+                'titleError' => '',
+                'descriptionError' => '',
+                'addressError' => '',
+                'contactError' => '',
+                'start_dateError' => '',
+                'end_dateError' => '',
+                'budgetError' => '',
+                'workError' => '',
+
+            ];
+
+            if(empty($data['category'])){
+                $data['categoryError'] = "The category of a Post cannot be empty";
+            }
+
+            if(empty($data['title'])){
+                $data['titleError'] = "The Title of a Post cannot be empty";
+            }
+
+            if(empty($data['description'])){
+                $data['descriptionError'] = "The description of a Post cannot be empty";
+            }
+
+            if(empty($data['address'])){
+                $data['addressError'] = "The address of a Post cannot be empty";
+            }
+
+            if(empty($data['contact'])){
+                $data['contactError'] = "The contact of a Post cannot be empty";
+            }
+
+            if(empty($data['start_date'])){
+                $data['start_dateError'] = "The start date of a Post cannot be empty";
+            }
+
+            if(empty($data['end_date'])){
+                $data['end_dateError'] = "The end date of a Post cannot be empty";
+            }
+
+            if(empty($data['budget'])){
+                $data['budgetError'] = "The budget of a Post cannot be empty";
+            }
+
+            if(empty($data['work'])){
+                $data['workError'] = "The work of a Post cannot be empty";
+            }
+
+            /*check error messages are empty*/
+            if(empty($data['categoryError']) && empty($data['titleError']) && empty($data['descriptionError']) && empty($data['addressError']) && empty($data['contactError']) && empty($data['start_dateError']) && empty($data['end_dateError']) && empty($data['budgetError']) && empty($data['workError'])){
+                if($this->companyModel->addPost($data)){
+                    header("Location: ". URLROOT . "/companies/company_ads"); //redirect to
+                }else{
+                    echo "<script>alert('Something went wrong, Please try again!'); </script>";
+                }
+            }else{
+                $this->view('companies/company_ads', $data);
+            }
+        }         
+          
+     $this->view('companies/company_ads', $data); 
      }
 
 
     //-----------------------------------------------------------------------------------------
     public function company_category() {
-        $this->view('companies/company_category', $data);      
+    $this->view('companies/company_category'/*, $data*/);      
     }
 
     public function company_dashboard() {
 
-        $this->view('companies/company_dashboard', $data);
+    $this->view('companies/company_dashboard'/*, $data*/);
 
     }
 
     public function company_inviteforjob() {
-        $this->view('companies/company_inviteforjob', $data);          
+    $this->view('companies/company_inviteforjob'/*, $data*/);          
     }
 
     public function company_profile() {
-        $this->view('companies/company_profile', $data);
+    $this->view('companies/company_profile'/*, $data*/);
           
     }
 
     public function company_profileforworkers() {
 
-        $this->view('companies/company_profileforworkers', $data);
+    $this->view('companies/company_profileforworkers'/*, $data*/);
           
     }
 
@@ -91,7 +190,7 @@ class Companies extends Controller {
 
     public function companyverify() {
 
-        $this->view('companies/companyverify', $data);
+    $this->view('companies/companyverify'/*, $data*/);
           
     }
 
--- app/models/Company.php
+++ app/models/Company.php
@@ -1,5 +1,5 @@
 <?php
-    class Review {
+    class Company {
         private $db;
 
         public function __construct() {
@@ -79,4 +79,40 @@
             }
         }
     
-    }
+    
+
+
+
+
+////// Company Ads ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     public function findAllPosts(){
+         $this->db->query('SELECT * FROM company_ads ORDER BY created_at DESC');
+
+         $results = $this->db->resultSet();
+
+         return $results;
+     }
+
+     public function addPost($data){
+        $this->db->query('INSERT INTO company_ads (reg_no,  category, title, description, address, contact, start_date, end_date, budget, work) VALUES (:reg_no, :category, :title, :description, :address, :contact, :start_date, :end_date, :budget, :work)');
+
+        //bind values
+        $this->db->bind(':reg_no', $data['reg_no']);
+        $this->db->bind(':category', $data['category']);
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':address', $data['address']);
+        $this->db->bind(':contact', $data['contact']);
+        $this->db->bind(':start_date', $data['start_date']);
+        $this->db->bind(':end_date', $data['end_date']);
+        $this->db->bind(':budget', $data['budget']);
+        $this->db->bind(':work', $data['work']);
+
+        //execute the function
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+     }
+}
