a654f4e2789b663abeeac55c500db396c273d004
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2022-01-20 01:53:53 +0530

    admin add-categories into the table

--- app/controllers/Admins.php
+++ app/controllers/Admins.php
@@ -1,22 +1,67 @@
 <?php
     class Admins extends Controller {
         public function __construct() {
-            //$this->pageModel = $this->model('Page');
+            $this->adminModel = $this->model('Admin');
         }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public function admin_category() {
 
-            //$users = $this->pageModel-> getUsers();
-            $data = [
-                'title' => 'admin_category page',
-                //'users' => $users
+            // if(!isLoggedIn()){
+            //     header("Location: " . URLROOT . "/admins");
+            // }
 
+            $add = $this->adminModel->findAllCategory();
+
+            $data = [
+                'add'  => $add,
+                'category' => '',
+                'logo' => '',
+                'add_date' => '',
+                'categoryError' => '',
+                'logoError' => ''
             ];
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); 
+
+                $data = [
+                    'add'  => $add,
+                    // 'admin_id' => $_SESSION['admin_id'],
+                    'category' => trim($_POST['category']),
+                    'logo' => trim($_POST['logo']),
+                    'categoryError' => '',
+                    'logoError' => ''
+                 ];
+
+                if(empty($data['category'])){
+                    $data['categoryError'] = 'The category field cannot be empty';
+                }
+                if(empty($data['logo'])){
+                    $data['logoError'] = 'The image upload field cannot be empty';
+                }
+                if(empty($data['categoryError']) && empty($data['logoError'])){
+                    if ($this->adminModel->addCategory($data)){
+                        header("Location: " . URLROOT . "/admins/admin_category");
+                        
+                    }else{
+                        die("Something went wrong, please try again");
+                    }
+                }else{
+                    $this->view('admins/admin_category', $data);
+                }
+            }
 
             $this->view('admins/admin_category', $data);
           
         }
 
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public function admin_complaints() {
 
             //$users = $this->pageModel-> getUsers();
