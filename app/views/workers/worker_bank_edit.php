faf4f13cb59318622ed4ad2a1ac1ebe09b7a5f2f
Nitharsha99 <88332583+Nitharsha99@users.noreply.github.com>
2022-03-22 20:56:47 +0530

    update bank edit page

--- app/views/workers/worker_bank_edit.php
+++ app/views/workers/worker_bank_edit.php
@@ -9,13 +9,16 @@
 <div class="category" id="my-profile" style="margin-top: -15em"> <p> Bank Details </p> </div>
 
 <div class="form-space"->
-    <form action="/action_page.php">
+    <form action="<?php echo URLROOT; ?>/workers/worker_bank_edit" method="POST">
         <div class="row">
             <div class="col-25">
             <label for="name">Name</label>
             </div>
             <div class="col-75">
-            <input type="text" id="name" name="name" placeholder="Enter Your Name" required>
+            <input type="text" id="name" name="name" value="<?php echo $data['bankdata']->holder; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['nameError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -24,7 +27,10 @@
             <label for="b-name">Bank Name</label>
             </div>
             <div class="col-75">
-            <input type="text" id="b-name" name="b-name" placeholder="Enter Your Bank Name" required>
+            <input type="text" id="bname" name="bname" value="<?php echo $data['bankdata']->bank_name; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['bnameError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -33,7 +39,10 @@
             <label for="branch">Bank Branch</label>
             </div>
             <div class="col-75">
-            <input type="text" id="branch" name="branch" placeholder="Enter Your Bank Branch" required>
+            <input type="text" id="branch" name="branch" value="<?php echo $data['bankdata']->branch; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['branchError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -42,7 +51,10 @@
             <label for="code">Bank Code</label>
             </div>
             <div class="col-75">
-            <input type="text" id="code" name="code" placeholder="Enter Your Bank Code" required>
+            <input type="text" id="code" name="code" value="<?php echo $data['bankdata']->code; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['codeError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -51,13 +63,16 @@
             <label for="account">Account Number</label>
             </div>
             <div class="col-75">
-            <input type="text" id="account" name="account" placeholder="Enter Your Account number" required>
+            <input type="text" id="account" name="account" value="<?php echo $data['bankdata']->account_no; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['accountError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
         </br>
         <div class="row">
-            <input type="submit" value="Update" class="green-button">
+            <button type="submit" class="green-button">Update</button>
         </div>
     </form>
 </div>
