8ae3d351216acf44dc1162a88a4ffe72d419a68b
Nitharsha99 <88332583+Nitharsha99@users.noreply.github.com>
2022-03-24 19:41:12 +0530

    delete change password from profile page

--- app/views/workers/worker_profile_edit.php
+++ app/views/workers/worker_profile_edit.php
@@ -67,56 +67,6 @@
     </form>
 </div>
 
-<!----------------Change Password--------------------------------------->
-<div class="heading" id="change-password"> <p> Change Password </p> </div>
-
-<div class="form-space">
-    <form action="<?php echo URLROOT; ?> /workers/password_edit" method = "POST">
-        <div class="row">
-            <div class="col-25">
-            <label for="password">Password</label>
-            </div>
-            <div class="col-75">
-            <input type="password" id="wpassword" name="wpassword" placeholder="Enter your Password" required>
-            </br> </br>
-                <span class="invalidFeedback">
-                    <?php echo $data['wpasswordError']; ?>
-                </span>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-25">
-            <label for="new-password">New Password</label>
-            </div>
-            <div class="col-75">
-            <input type="password" id="new-password" name="new-password" placeholder="Enter New Password" required>
-            </br> </br>
-                <span class="invalidFeedback">
-                    <?php echo $data['new-passwordError']; ?>
-                </span>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-25">
-            <label for="confirm-password">Confirm Password</label>
-            </div>
-            <div class="col-75">
-            <input type="password" id="confirm-password" name="confirm-password" placeholder="Renter New Password" required>
-            </br> </br>
-                <span class="invalidFeedback">
-                    <?php echo $data['confirm-passwordError']; ?>
-                </span>
-            </div>
-        </div>
-        </br>
-        <div class="row">
-            <input type="submit" value="Change Password" class="green-button">
-        </div>
-    </form>
-</div>
-
 <!-----------------------------------Edit Service------------------------------------------------>
 
 <div class="heading" id="my-service"> <p> My Service </p> </div>
