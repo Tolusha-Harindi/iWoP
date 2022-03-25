b02700d3990c418b434191ced7c851a3fbc9bdc8
Nitharsha99 <88332583+Nitharsha99@users.noreply.github.com>
2022-03-26 00:52:49 +0530

    Update worker_new_profile.php

--- app/views/workers/worker_new_profile.php
+++ app/views/workers/worker_new_profile.php
@@ -8,21 +8,31 @@
 
 <div class="category" id="my-profile"> <p> My Profile </p> </div>
 
-<div id="profile-container">
-    <image id="profileImage" src="<?php echo URLROOT;?> /public/img/profile.jpg" />
-</div>
-<input id="imageUpload" type="file"  name="profile_photo" placeholder="Photo" required="" capture>
-<p style="margin-left:54.5%; color: lightgrey; font-size:13px;"><i>Click the image to upload pic</i></p>
 <script type="text/javascript" src="<?php echo URLROOT;?> /public/js/pic.js"></script>
 
 <div class="form-space">
-    <form action="/action_page.php">
+    <form action="<?php echo URLROOT;?> /workers/worker_new_profile" method="POST">
+    <div id="profile-container" style="margin-left:40%; margin-top:10px;">
+    <image id="profileImage" src="<?php echo URLROOT;?> /public/img/profile.jpg" />
+    </div>
+
+    <div class="col-75" style="margin-left:18em;">
+            <input type="file" id="prof_pic" name="prof_pic">
+            </br> </br>
+                <span class="invalidFeedback">
+                    <?php echo $data['profileError']; ?>
+                </span>
+        </div>
+
         <div class="row">
             <div class="col-25">
             <label for="name">Name</label>
             </div>
             <div class="col-75">
-            <input type="text" id="name" name="name" placeholder="Enter Your Name" required>
+            <input type="text" id="name" name="name" value="<?php echo $data['worker']->fname; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['nameError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -31,16 +41,10 @@
             <label for="email">Email</label>
             </div>
             <div class="col-75">
-            <input type="email" id="email" name="email" placeholder="Enter Your Email" required>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-25">
-            <label for="contact">Contact Number</label>
-            </div>
-            <div class="col-75">
-            <input type="text" id="contact" name="contact" placeholder="Enter Your Contact Number" required>
+            <input type="email" id="email" name="email" value="<?php echo $data['worker']->email; ?>" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['emailError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -50,6 +54,9 @@
             </div>
             <div class="col-75">
             <input type="text" id="address" name="address" placeholder="Enter Your address" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['addressError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -59,6 +66,9 @@
             </div>
             <div class="col-75">
             <input type="text" id="district" name="district" placeholder="Enter Your District" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['districtError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -68,15 +78,9 @@
             </div>
             <div class="col-75">
             <input type="text" id="city" name="city" placeholder="Enter Your City" required>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-25">
-            <label for="nic">NIC Number</label>
-            </div>
-            <div class="col-75">
-            <input type="text" id="nic" name="nic" placeholder="Enter Your NIC Number" required>
+            <span class="invalidFeedback">
+                <?php  echo $data['cityError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
@@ -87,10 +91,16 @@
             <div class="col-75">
             <label for="front" style="margin-left: 0">Front Side</label><br/>
             <input type="file" id="front" name="front" required style="background-color: white">
+            <span class="invalidFeedback">
+                <?php  echo $data['frontsideError'];?>
+            </span> </br> </br>
             </div>
             <div class="col-75" style="margin-left: 180px; margin-top: 0.8em;">
             <label for="back" style="margin-left: 0">Back Side</label></br>
             <input type="file" id="back" name="back" required style="background-color: white">
+            <span class="invalidFeedback">
+                <?php  echo $data['backsideError'];?>
+            </span> </br> </br>
             </div>
         </div>
 
