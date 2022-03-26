2b6b1e1aae9fe3e35bd147ab9dd4c8fc2308f88c
Tolusha Harindi <62212997+Tolusha-Harindi@users.noreply.github.com>
2022-03-26 23:09:03 +0530

    admin side nave updated

--- app/views/includes/admin_sidenav.php
+++ app/views/includes/admin_sidenav.php
@@ -15,11 +15,7 @@
                 <img src="<?php echo URLROOT ?>/public/img/admin.jpg ?>"  class="img1"/>
         </div>
 
-        <!-- <?php foreach($data['adminName'] as $adminName): ?>
-                <p class='title'><?php echo $adminName->name; ?></p>
-        <?php endforeach; ?>  -->
-
-        <p class='title'>Tolushaa Harindi</p>
+        <p class='title'><?php echo $_SESSION['name']?></p>
 
             
             <div class="description">
