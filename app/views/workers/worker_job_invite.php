7ca1b5c1706aed6fa70c78c14387908648721727
Nitharsha99 <88332583+Nitharsha99@users.noreply.github.com>
2022-03-27 19:25:45 +0530

    Update worker_job_invite.php

--- app/views/workers/worker_job_invite.php
+++ app/views/workers/worker_job_invite.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <!-------------------------------------job invite table--------------------------------------->
-<div class="category" id="my-profile" style="margin-top: -14em;"> <p>Job Invites</p> </div>
+<div class="category" id="my-profile" style="margin-top: -14em;"> <p>Customer Job Invites</p> </div>
 
 <div class="table">
     <div class="table-wrapper" style="margin-top:2em;">
@@ -18,53 +18,22 @@
                 <tr>
                     <th>Name</th>
                     <th>Profile Pic</th>
-                    <th>Category</th>
+                    <th>Invite Time</th>
+                    <th>Contact Number</th>
                     <th>More</th>
-                    <th>Message</th>
                 </tr>
             </thead>
 
             <tbody>
-                
+            <?php foreach($data['jobs'] as $jobs): ?>   
                 <tr>
-                    <td>Vishnu Gowtham</td>
-                    <td><img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
-                    <td>Plumber</td>
-                    <td> <button  id="myBtn" class="blue-out-button">More </button></td>
-                    <td> <input type="reset" class="blue-out-button" value="Message"/></td>
+                    <td><?php echo $jobs->fname . " " . $jobs->lname;?></td>
+                    <td><img src="<?php $jobs->prof_pic;?>" class="table-image" style="border-radius: 50px;"></td>
+                    <td><?php echo $jobs->invite_at;?></td>
+                    <td><?php echo $jobs->contact;?></td>
+                    <td> <a href="<?php echo URLROOT . "/workers/show_job_detail/". $jobs->job_id;?>"><button  id="myBtn" class="blue-out-button">More </button></a></td>
                 </tr>
-
-                <tr>
-                    <td>Vishnu Gowtham</td>
-                    <td><img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
-                    <td>Plumber</td>
-                    <td> <button  id="myBtn" class="blue-out-button">More </button></td>
-                    <td> <input type="reset" class="blue-out-button" value="Message"/></td>
-                </tr>
-
-                <tr>
-                    <td>Vishnu Gowtham</td>
-                    <td><img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
-                    <td>Plumber</td>
-                    <td> <button  id="myBtn" class="blue-out-button">More </button></td>
-                    <td> <input type="reset" class="blue-out-button" value="Message"/></td>
-                </tr>
-
-                <tr>
-                    <td>Vishnu Gowtham</td>
-                    <td><img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
-                    <td>Plumber</td>
-                    <td> <button  id="myBtn" class="blue-out-button">More </button></td>
-                    <td> <input type="reset" class="blue-out-button" value="Message"/></td>
-                </tr>
-
-                <tr>
-                    <td>Vishnu Gowtham</td>
-                    <td><img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 50px;"></td>
-                    <td>Plumber</td>
-                    <td> <button  id="myBtn" class="blue-out-button">More </button></td>
-                    <td> <input type="reset" class="blue-out-button" value="Message"/></td>
-                </tr>
+                <?php endforeach; ?>
             </tbody>
         </table> 
     </div> 
@@ -72,82 +41,38 @@
 
 <br/><br/>
 
+<!-------------------------------------job invite table--------------------------------------->
+<div class="category" id="my-profile" style="margin-top: 1em;"> <p>Company Job Invites</p> </div>
 
-<!-------------------------------------------------popup message--------------------------------------->
-<div id="myModal" class="modal">
-        <!-- Modal content -->
-        <div class="modal-content" style="margin-top: 7%;">
-                <form action="">
-                    <span class="close" style="margin-top:0.2em;">&times;</span>
-                    <img src="<?php echo URLROOT;?> /public/img/w3.jpg" class="table-image" style="border-radius: 200px; height:50px; width: 50px; margin-left:5%; margin-top:1em;">
-                    <p class="heading" style="margin-top: -0.5em; margin-left:40%;">Job Invites</p>
-                        <div class="row" style="margin-left:3em;">
-                            <div class="col-25" style="margin-top:0.7em; width: 30%;">
-                            <label for="fname" style="color: #0526BA;">Name</label>
-                            </div>
-                            <div class="col-75" style="margin-top:0.7em; width: 65%;">
-                            <p style="color: #0526BA;">Amuthan Vikram</p>
-                            </div>
-                        </div>
+<div class="table">
+    <div class="table-wrapper" style="margin-top:2em;">
+        <table style="border-spacing: 25px" class="fl-table">
+            <thead>
+                <tr>
+                    <th>Company Name</th>
+                    <th>Profile Pic</th>
+                    <th>Invite Time</th>
+                    <th>Contact Number</th>
+                    <th>More</th>
+                </tr>
+            </thead>
 
-                        <div class="row" style="margin-left:3em;">
-                            <div class="col-25" style="margin-top:0.7em; width: 30%;">
-                            <label for="lname" style="color: #0526BA;">Contact Number</label>
-                            </div>
-                            <div class="col-75" style="margin-top:0.7em; width: 65%;">
-                            <p style="color: #0526BA;">076 245 5387</p>
-                            </div>
-                        </div>
-                        
-                        <div class="row" style="margin-left:3em;">
-                            <div class="col-25" style="margin-top:0.7em; width: 30%;">
-                            <label for="contact" style="color: #0526BA;">Time</label>
-                            </div>
-                            <div class="col-75" style="margin-top:0.7em; width: 65%;">
-                            <p style="color: #0526BA;">08.00 a.m.</p>
-                            </div>
-                        </div>
+            <tbody>
+            <?php foreach($data['com'] as $jobs): ?>   
+                <tr>
+                    <td><?php echo $jobs->com_name;?></td>
+                    <td><img src="<?php $jobs->prof_pic;?>" class="table-image" style="border-radius: 50px;"></td>
+                    <td><?php echo $jobs->invite_at;?></td>
+                    <td><?php echo $jobs->contact;?></td>
+                    <td> <a href="<?php echo URLROOT . "/workers/show_job_Com_detail/". $jobs->job_id;?>"><button  id="myBtn" class="blue-out-button">More </button></a></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table> 
+    </div> 
+</div>
 
-                        <div class="row" style="margin-left:3em;">
-                            <div class="col-25" style="margin-top:0.7em; width: 30%;">
-                            <label for="amount" style="color: #0526BA;">Start Date</label>
-                            </div>
-                            <div class="col-75" style="margin-top:0.7em; width: 65%;">
-                            <p style="color: #0526BA;">29/11/2021</p>
-                            </div>
-                        </div>
-
-                        <div class="row" style="margin-left:3em;">
-                            <div class="col-25" style="margin-top:0.7em; width: 30%;">
-                            <label for="contact" style="color: #0526BA;">Address</label>
-                            </div>
-                            <div class="col-75" style="margin-top:0.7em; width: 65%;">
-                            <p style="color: #0526BA;">37, Central Road, Kalladi, Batticallo</p>
-                            </div>
-                        </div>
-
-                        <div class="row" style="margin-left:3em;">
-                            <div class="col-25" style="margin-top:0.7em; width: 30%;">
-                            <label for="contact" style="color: #0526BA;">Budget</label>
-                            </div>
-                            <div class="col-75" style="margin-top:0.7em; width: 65%;">
-                            <p style="color: #0526BA;">3000.00</p>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-25" style="margin-left: 30%; width: 15%;">
-                                <input type="submit" value="Accept" class="blue-button" style="margin-left:45%;">
-                            </div>
-                            <div class="col-75" style="width: 15%;">
-                                 <input type="submit" value="Reject" class="pink-button" style="margin-left:53%; margin-top:0em; padding: 8px 25px;">
-                            </div>
-                        </div>
-                        </br>
-                </form>
-        </div>
-    </div>
-
+    
 <script type="text/javascript" src="<?php echo URLROOT;?> /public/js/blue-out-button-popup.js"></script>
 
 
